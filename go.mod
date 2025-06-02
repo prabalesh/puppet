da54a7df6cceef600b93fea2f421d7ed05975852
Prabalesh <prabalesh2008@gmail.com>
2025-06-03 00:32:46 +0530

    feat: create languages table
    - create languages table with fields for name, version, image name, installed, timestamps

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/prabalesh/puppet
 
 go 1.23.4
 
-require github.com/mattn/go-sqlite3 v1.14.28 // indirect
+require github.com/mattn/go-sqlite3 v1.14.28 // direct
--- main.go
+++ main.go
@@ -18,7 +18,21 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to connect to database: %v", err.Error())
 	}
-	db.Close()
+	defer db.Close()
+
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS languages (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name TEXT,
+			version TEXT,
+			image_name TEXT UNIQUE,
+			installed BOOLEAN,
+			created_at DATETIME,
+			updated_at DATETIME
+		)`)
+	if err != nil {
+		log.Fatalf("failed to create the languages table%v", err.Error())
+	}
 
 	// server listenting
 	mux := http.NewServeMux()
