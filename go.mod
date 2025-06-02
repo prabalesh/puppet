5fe149ce766f895dbbdef727b0a5da6d945e8705
Prabalesh <prabalesh2008@gmail.com>
2025-06-03 00:26:39 +0530

    feat: create basic server and database connection setup
    - create database connection
    - create basic http server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/prabalesh/puppet
 
 go 1.23.4
+
+require github.com/mattn/go-sqlite3 v1.14.28 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
+var db *sql.DB
+
 func main() {
-	fmt.Println("Basic web server")
+	// database connection
+	var err error
+	db, err = sql.Open("sqlite3", "file:storage/languages.db?cache=shared&_foreign_keys=on")
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err.Error())
+	}
+	db.Close()
+
+	// server listenting
+	mux := http.NewServeMux()
+
+	// handlers
+
+	fmt.Println("Server started at http://localhost:8080/")
+	if err = http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatalf("failed to start server: %v\n", err.Error())
+		return
+	}
 }
