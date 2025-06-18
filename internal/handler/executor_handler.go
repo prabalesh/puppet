e822b6872546698557fdf70641f39b2ab6036f81
Prabalesh <prabalesh2008@gmail.com>
2025-06-18 21:49:30 +0530

    feat(handler/executor): implement RunCode endpoint to execute code via service

--- internal/handler/executor_handler.go
+++ internal/handler/executor_handler.go
@@ -1,8 +1,11 @@
 package handler
 
 import (
+	"encoding/json"
 	"log/slog"
+	"net/http"
 
+	"github.com/prabalesh/puppet/internal/dto"
 	"github.com/prabalesh/puppet/internal/service"
 )
 
@@ -13,4 +16,22 @@ type ExecutorHandler struct {
 
 func NewExecutorHandler(s *service.ExecutorService, logger *slog.Logger) *ExecutorHandler {
 	return &ExecutorHandler{s, logger}
+}
+
+func (h *ExecutorHandler) RunCode(w http.ResponseWriter, r *http.Request) {
+	var runCodeReqBody dto.ExecuteCodeRequest
+	if err := json.NewDecoder(r.Body).Decode(&runCodeReqBody); err != nil {
+		h.logger.Warn("Invalid request payload", "error", err)
+		RespondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+
+	codeOutput, err := h.Server.RunCode(runCodeReqBody)
+	if err != nil {
+		h.logger.Error("Failed to run code", "error", err)
+		RespondWithError(w, http.StatusInternalServerError, "Failed to add language")
+		return
+	}
+
+	RespondWithJSON(w, http.StatusOK, codeOutput)
 }
