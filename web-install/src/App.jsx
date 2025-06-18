f9d076c49f747c63c7de661ee79e90cfc1aac839
Prabalesh <prabalesh2008@gmail.com>
2025-06-18 22:11:06 +0530

    refactor(ui): integrate react-router and split routes for language management and code execution

--- web-install/src/App.jsx
+++ web-install/src/App.jsx
@@ -1,14 +1,34 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { useState } from 'react';
 import LanguageForm from './components/LanguageForm';
 import LanguageList from './components/LanguageList';
+import CodeExecution from './pages/CodeExecution';
 
 export default function App() {
   const [refreshKey, setRefreshKey] = useState(0);
+
   return (
-    <div style={{ padding: '1rem' }}>
-      <h1>Puppet | Language Manager</h1>
-      <LanguageForm onSuccess={() => setRefreshKey(k => k + 1)} />
-      <LanguageList key={refreshKey} />
-    </div>
+    <Router>
+      <div style={{ padding: '1rem' }}>
+        <h1>Puppet</h1>
+        <nav style={{ marginBottom: '1rem' }}>
+          <Link to="/" style={{ marginRight: 10 }}>Language Manager</Link>
+          <Link to="/executions">Run Code</Link>
+        </nav>
+
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <LanguageForm onSuccess={() => setRefreshKey(k => k + 1)} />
+                <LanguageList key={refreshKey} />
+              </>
+            }
+          />
+          <Route path="/executions" element={<CodeExecution />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
