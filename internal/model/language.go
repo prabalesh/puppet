28fa8266d96dc3f6bfe52d10bae2222b1864b245
Prabalesh <prabalesh2008@gmail.com>
2025-06-19 18:19:10 +0530

    feat(model): add FileName, CompileCommand, and RunCommand to Language struct

--- internal/model/language.go
+++ internal/model/language.go
@@ -3,11 +3,14 @@ package model
 import "time"
 
 type Language struct {
-	ID        int       `json:"id"`
-	Name      string    `json:"name"`
-	Version   string    `json:"version"`
-	ImageName string    `json:"image_name"`
-	Installed bool      `json:"installed"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID             int       `json:"id"`
+	Name           string    `json:"name"`
+	Version        string    `json:"version"`
+	ImageName      string    `json:"image_name"`
+	FileName       string    `json:"file_name"`
+	CompileCommand string    `json:"compile_command"`
+	RunCommand     string    `json:"run_command"`
+	Installed      bool      `json:"installed"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
 }
