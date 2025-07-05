1df6600da3527ff37b55b064b73a79e7905d7793
Prabalesh <prabalesh2008@gmail.com>
2025-07-05 15:14:37 +0530

    feat(ui): improve header layout with branding and updated styles

--- web-install/src/App.jsx
+++ web-install/src/App.jsx
@@ -10,15 +10,17 @@ export default function App() {
     return (
         <Router>
             <div>
-                <header className="mb-5 h-[10vh] bg-slate-600 text-white text-2xl flex justify-around items-center">
+                <header className="mb-5 h-[10vh] bg-white border-b-1 font-bold text-purple-600 text-2xl flex justify-around items-center">
                     <div>
-                        <h1>Puppet</h1>
+                        <Link to={"/"}>
+                            <img src="./puppet.png" className="h-36 w-36" alt="Puppet" />
+                        </Link>
                     </div>
-                    <nav className="flex gap-4 text-xl">
-                        <Link to="/" style={{ marginRight: 10 }}>
+                    <nav className="flex gap-6 text-xl">
+                        <Link to="/" className="hover:text-purple-800">
                             Language Manager
                         </Link>
-                        <Link to="/executions">Run Code</Link>
+                        <Link to="/executions" className="hover:text-purple-800">Run Code</Link>
                     </nav>
                 </header>
 
